136aa002cc6a7cf8849c47e9ddffde2aa22123f3
Curtis Frazier <fraziercurtis2003@yahoo.com>
2017-07-19 16:06:17 -0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <div className="Title">
+      <div className="my-header">
+      <p>Earthquakes!!!</p>
+        </div>
+      </div>
+    </div>
+    );
+  }
+}
+
+export default App;
